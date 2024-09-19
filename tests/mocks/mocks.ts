92979cca5350ba579a25b6e39d7d459310ac55bd
julienlucas <julienlucas-ext@smartch.fr>
2024-09-19 15:06:42 +0200

    Fix tests

---
+++ tests/mocks/mocks.ts
@@ -0,0 +1,24 @@
+import { trainingsFakeData } from '@tests/fixtures/trainings.fakedata';
+import { faker } from '@faker-js/faker';
+
+export const firstNameMock = faker.person.firstName();
+export const lastNameMock = faker.person.lastName();
+// export const passwordMock = faker.internet.password({
+//   pattern: new RegExp('^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[*.!@$%^&(){}[\\]:;<>,.?/~_+-=|]).{8,32}$')
+// });
+
+export const passwordMock = "superP4ssword#";
+export const emailMock = faker.internet.email({
+  firstName: firstNameMock,
+  lastName: lastNameMock,
+  allowSpecialCharacters: false
+});
+
+// Mock for user already in database
+export const userMock = {
+  id: trainingsFakeData[0].authorId,
+  password: "superP4ssword#",
+  email: "hello@julienlucas.com",
+  createdAt: new Date(),
+  updatedAt: new Date(),
+};
